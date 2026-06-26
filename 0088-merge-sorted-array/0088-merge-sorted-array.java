01a05aef4e83e0051264a9332350b128079d5bf1
Aman Rawat <aman17rawat2005@gmail.com>
2026-06-26 12:34:08 +0530

    Time: 0 ms (100%), Space: 43.6 MB (87.97%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,28 +1,19 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i = 0;
-        int j = 0;
-        int[] temp = new int[m+n];
-        int k = 0;
+        int idx = m + n -1;
+        int i = m - 1;
+        int j = n - 1;
 
-        while(i < m && j < n){
-            if(nums1[i] <= nums2[j]){
-                temp[k++] = nums1[i++];
+        while(i>=0 && j>=0){
+            if(nums1[i] > nums2[j]){
+                nums1[idx--] = nums1[i--];
             }
             else{
-                temp[k++] = nums2[j++];
+                nums1[idx--] = nums2[j--];
             }
         }
-
-        while(i < m){
-            temp[k++] = nums1[i++];
-        }
-        while(j < n){
-            temp[k++] = nums2[j++];
-        }
-
-        for(int idx=0;idx<temp.length;idx++){
-            nums1[idx] = temp[idx];
+        while(j>=0){
+            nums1[idx--] = nums2[j--];
         }
     }
 }
@@ -31,20 +22,29 @@ class Solution {
 
 // class Solution {
 //     public void merge(int[] nums1, int m, int[] nums2, int n) {
-//         int idx = m + n -1;
-//         int i = m - 1;
-//         int j = n - 1;
+//         int i = 0;
+//         int j = 0;
+//         int[] temp = new int[m+n];
+//         int k = 0;
 
-//         while(i>=0 && j>=0){
-//             if(nums1[i] > nums2[j]){
-//                 nums1[idx--] = nums1[i--];
+//         while(i < m && j < n){
+//             if(nums1[i] <= nums2[j]){
+//                 temp[k++] = nums1[i++];
 //             }
 //             else{
-//                 nums1[idx--] = nums2[j--];
+//                 temp[k++] = nums2[j++];
 //             }
 //         }
-//         while(j>=0){
-//             nums1[idx--] = nums2[j--];
+
+//         while(i < m){
+//             temp[k++] = nums1[i++];
+//         }
+//         while(j < n){
+//             temp[k++] = nums2[j++];
+//         }
+
+//         for(int idx=0;idx<temp.length;idx++){
+//             nums1[idx] = temp[idx];
 //         }
 //     }
 // }
