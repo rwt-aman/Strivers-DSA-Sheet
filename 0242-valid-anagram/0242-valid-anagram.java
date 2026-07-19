92a957811d1d88b8c21ec89ddf7a39a5c6e6365a
Aman Rawat <aman17rawat2005@gmail.com>
2026-07-19 23:16:25 +0530

    Time: 17 ms (16.57%), Space: 46.5 MB (34.37%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,38 +1,37 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        char[] schars = s.toCharArray();
-        char[] tchars = t.toCharArray();
+        if(s.length() != t.length()) return false;
 
-        Arrays.sort(schars);
-        Arrays.sort(tchars);
+        HashMap<Character,Integer> map = new HashMap<>();
 
-        return Arrays.equals(schars,tchars);
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        }
+
+        for(int i=0;i<t.length();i++){
+            char ch = t.charAt(i);
+
+            if(!map.containsKey(ch)) return false;
+
+            map.put(ch,map.get(ch)-1);
+            if(map.get(ch) == 0){
+                map.remove(ch);
+            }
+        }
+        return map.isEmpty();
     }
 }
 
 
-
 // class Solution {
 //     public boolean isAnagram(String s, String t) {
-//         if(s.length() != t.length()) return false;
+//         char[] schars = s.toCharArray();
+//         char[] tchars = t.toCharArray();
 
-//         HashMap<Character,Integer> map = new HashMap<>();
+//         Arrays.sort(schars);
+//         Arrays.sort(tchars);
 
-//         for(int i=0;i<s.length();i++){
-//             char ch = s.charAt(i);
-//             map.put(ch,map.getOrDefault(ch,0)+1);
-//         }
-
-//         for(int i=0;i<t.length();i++){
-//             char ch = t.charAt(i);
-
-//             if(!map.containsKey(ch)) return false;
-
-//             map.put(ch,map.get(ch)-1);
-//             if(map.get(ch) == 0){
-//                 map.remove(ch);
-//             }
-//         }
-//         return map.isEmpty();
+//         return Arrays.equals(schars,tchars);
 //     }
 // }
